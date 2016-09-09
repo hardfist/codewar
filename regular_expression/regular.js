b5a5baadef26fae86f1db8d3ea851fcba5618713
shengju.yj <shengju.yj@alibaba-inc.com>
2016-09-09 15:36:09 +0800

    adding more

--- regular_expression/regular.js
+++ regular_expression/regular.js
@@ -2,7 +2,7 @@ function Test() {
 }
 Test.assertEquals = function (lhs, rhs) {
 
-}
+};
 function calculate(string) {
     // Add your code here
     return eval(string.match(/\d+|lose|gain/g).join("").replace('lose', '-').replace('gain', '+'));
@@ -111,5 +111,95 @@ function onePunch(items){
         .map(item => item.replace(/[ae]/g,''))
         .join(' ')
 }
-var res = onePunch('Beard Knife Grenade Motorbike Hat');
-console.log('res:',res);
+function createSequence(regex){
+    return Array.from({length:256},(_,i)=>i).map(item => String.fromCharCode(item)).filter(item =>regex.test(item)).join('');
+}
+
+function createTemplate(template){
+    return scope => {
+        return template.replace(/\{\{([^}]+)\}\}/g,(w,$1) =>{
+            let val = scope[$1];
+            if(val === undefined){
+                return "";
+            }else{
+                return val;
+            }
+        })
+    }
+}
+function doubleCheck(str){
+    //Are there doubles?
+    return /(.)(?=\1)/.test(str);
+}
+
+function password(str){
+    return /^.*(?=[A-Z])(?=[a-z])(?=\d).*$/.test(str) && /.{8,}/.test(str);
+}
+
+String.prototype.toCents=function(){
+    //your code here
+    return this.replace(/$(\d+.?\d+)/,(w,$1) => (+$1)*100);
+};
+Array.prototype.filter = function(pred,context){
+    let res = [];
+    for(var i=0;i<this.length;i++){
+        if(typeof  pred != 'function'){
+            throw new TypeError('pred is not function:',pred);
+        }
+        if(pred.call(context,this[i],i,this)){
+            res.push(this[i]);
+        }
+    }
+    return res;
+};
+function peteTalk(speech,ok){
+    //your code here
+    return speech.replace(/([^.!?]*)([.!?])?/g,(str,$1,$2) =>{
+        console.log('str:',str);
+        return str.replace(/\b(\w)(\w*)(\w)\b/g,(w,$1,$2,$3) => {
+            if(ok.includes(w)) return w;
+            return $1+(new Array($2.length + 1).join('*'))+$3
+        }).toLowerCase().replace(/^\s*(\w)/,(w,$1) => w.toUpperCase())
+    })
+
+}
+function trumpDetector(trumpySpeech){
+    var sum  =0;
+    var cnt = 0;
+    trumpySpeech.replace(/a+|o+|e+|i+|u+/gi,w =>{
+        if(w.length>2){
+            sum += w.length-1;
+            cnt++;
+        }
+    });
+    return  sum/cnt;
+}
+String.prototype.toInteger = function(){
+    console.log(`this:${this}`)
+    var reg = /^([-+])?(0b|0x|0o)?([0-9a-fA-F]*)$/;
+    var match = this.match(reg);
+    console.log(`match:${match}`)
+    if(match == null){
+        return null;
+    }else{
+
+        var str = (match[1]|| '')+(match[3] || '');
+        console.log(`str:${str}`);
+        switch(match[2]){
+            case '0b':
+                if(/[^01]/.test(match[3])) return null;
+                return parseInt(str,2);
+            case '0x':
+                if(/[^0-9a-fA-F]/.test(match[3])) return null;
+                return parseInt(str,16);
+            case '0o':
+                if(/[^0-7]/.test(match[3])) return null;
+                return parseInt(str,8);
+            default:
+                console.log(`match[3]:${match[3]}`);
+                if(/[^0-9]/.test(match[3])) return null;
+                return parseInt(str,10);
+        }
+    }
+};
+console.log('0o18'.toInteger());
